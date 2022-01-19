e1d6dca7bbdfe56350863c7b6b2faf6e46b0f65b
VictorDauph <victor.dauph@gmail.com>
2022-01-19 17:27:13 +0100

    corrections header et fautes textes

--- style/JS/headerSticky.js
+++ style/JS/headerSticky.js
@@ -11,14 +11,16 @@ var sticky = navbar.offsetTop;
 function stickNavbar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
-    navbar.classList.add("bg-opacity-75")
-    navbar.classList.remove("bg-opacity-50")
+    //navbar.classList.add("bg-opacity-75") //Ces lignes de codes servaient dans une version précédente où la transparence du ba&ckground dépendait de sa position dans la page
+    //navbar.classList.remove("bg-opacity-50")
+    navbar.classList.remove("my-5")
     navbar.classList.add("anim-navbar")
     navbar.classList.remove("anim-navbar-reverse")
   } else {
     navbar.classList.remove("sticky");
-    navbar.classList.add("bg-opacity-50")
-    navbar.classList.remove("bg-opacity-75")
+    //navbar.classList.add("bg-opacity-50")
+    //navbar.classList.remove("bg-opacity-75")
+    navbar.classList.add("my-5")
     navbar.classList.remove("anim-navbar")
     navbar.classList.add("anim-navbar-reverse")
   }
